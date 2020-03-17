9a076d28b872969b62353454979524bf2bfb3f58
mullwar <mullwar@protonmail.com>
2020-03-17 02:37:48 +0200

    🚀 Update bot using telebot 2.0-pre

--- bot.js
+++ bot.js
@@ -1,52 +1,64 @@
-const TeleBot = require('telebot');
-const translate = require('google-translate-api');
+const {TeleBot} = require("telebot");
+const translate = require("translation-google");
 
 const {
     TELEBOT_TOKEN: TOKEN,
     TELEBOT_URL: URL,
     TELEBOT_HOST: HOST,
-    TELEBOT_PORT: PORT,
+    PORT
 } = process.env;
 
-const TRANSLATE_TO = 'en';
+const TRANSLATE_TO = "en";
 const TRANSLATE_ICONS = {
-    ru: '🇷🇺', lv: '🇱🇻', ua: '🇺🇦', uz: '🇺🇿', fr: '🇫🇷',
-    es: '🇪🇸', it: '🇮🇹', nl: '🇳🇱'
+    ru: "🇷🇺",
+    lv: "🇱🇻",
+    ua: "🇺🇦",
+    uz: "🇺🇿",
+    fr: "🇫🇷",
+    es: "🇪🇸",
+    it: "🇮🇹",
+    nl: "🇳🇱"
 };
 
 const bot = new TeleBot({
     token: TOKEN,
-    url: URL,
-    host: HOST,
-    port: PORT
+    webhook: {
+        url: URL,
+        host: HOST,
+        port: PORT
+    },
+    allowedUpdates: ["message"]
 });
 
-bot.on('text', (msg) => {
-
-    const text = msg.text;
-    const chatId = msg.chat.id;
-    const messageId = msg.message_id;
-
-    console.log('+', chatId, messageId, text);
-
-    return translate(text, {to: TRANSLATE_TO}).then((response) => {
-
-        const translatedText = response.text;
-        const languageId = response.from.language.iso;
-
-        if (languageId !== TRANSLATE_TO) {
-
-            const languageIcon = TRANSLATE_ICONS[languageId] || '';
-
-            return bot.sendMessage(chatId, `${languageIcon} ${translatedText}`, {
-                reply: messageId,
-                preview: false
-            });
-            
+bot.on("text", (message) => {
+    const {
+        message_id,
+        text,
+        chat: {
+            id: chat_id
+        },
+        from: {
+            id: from_id,
+            first_name
         }
+    } = message;
 
+    console.log(`${chat_id}: ${first_name} (${from_id}) - ${text}`);
+
+    return translate(text, {to: TRANSLATE_TO}).then(({text, from}) => {
+        const iso = from.language.iso;
+        if (iso !== TRANSLATE_TO) {
+            const icon = TRANSLATE_ICONS[iso] || "";
+            return bot.sendMessage(chat_id, `${icon} ${text}`, {
+                reply_to_message_id: message_id,
+                disable_notification: true,
+                disable_web_page_preview: true
+            });
+        }
     });
-
 });
 
-bot.start();
+bot.deleteWebhook().then(() => {
+    bot.start();
+});
+
