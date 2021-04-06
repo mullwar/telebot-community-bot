10d0f5e3c2a4d70c14f17a2972bfea5a2f960c80
mullwar <mullwar@protonmail.com>
2021-04-07 00:00:31 +0300

    Use country-code-emoji for emoji flags

--- src/translate.js
+++ src/translate.js
@@ -1,23 +1,14 @@
 const t = require("translatte");
+const {countryCodeEmoji} = require("country-code-emoji");
 
 const TRANSLATE_TO = "en";
-const TRANSLATE_ICONS = {
-    ru: "🇷🇺",
-    lv: "🇱🇻",
-    ua: "🇺🇦",
-    uz: "🇺🇿",
-    fr: "🇫🇷",
-    es: "🇪🇸",
-    it: "🇮🇹",
-    nl: "🇳🇱"
-};
 
 module.exports = {
     translate(value, translateTo = TRANSLATE_TO) {
         return t(value, {to: translateTo}).then(({text, from}) => {
             const iso = from.language.iso;
             if (iso !== translateTo) {
-                const icon = TRANSLATE_ICONS[iso];
+                const icon = countryCodeEmoji(iso);
                 return [icon, text].filter(Boolean).join(" ");
             }
             return null;
