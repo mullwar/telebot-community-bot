de26d2ca78835f3cac59e5b53268cd2988e3f86b
mullwar <mullwar@protonmail.com>
2021-04-07 00:26:16 +0300

    Drop pending updates, add / redirect

--- src/bot.js
+++ src/bot.js
@@ -21,7 +21,9 @@ bot.plugin(translator, {
 });
 
 bot.deleteWebhook().then(() => {
-    return bot.setWebhook(`${TELEBOT_WEBHOOK_URL}/bot/${TELEBOT_TOKEN}`);
+    return bot.setWebhook(`${TELEBOT_WEBHOOK_URL}/bot/${TELEBOT_TOKEN}`, {
+        drop_pending_updates: true
+    });
 });
 
 bot.on("error", (error) => console.error("ERROR", error));
@@ -29,6 +31,10 @@ bot.on("error", (error) => console.error("ERROR", error));
 app.post(`/bot/${TELEBOT_TOKEN}`, (request, response) => {
     bot.processTelegramUpdates([request.body]);
     response.sendStatus(200);
+});
+
+app.get("/", (request, response) => {
+    response.status(301).redirect("https://github.com/mullwar/telebot-community-bot")
 });
 
 app.listen(PORT || 80, () => console.log(
